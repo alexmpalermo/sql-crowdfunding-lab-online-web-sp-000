c1ec0dfd44f4e9acd5f602cca01513780d7e3e31
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-26 23:50:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,6 +23,7 @@ JOIN projects ON pledges.project_id = projects.id
 GROUP BY projects.id
 HAVING SUM(pledges.amount) >= projects.funding_goal  
 ;"
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
